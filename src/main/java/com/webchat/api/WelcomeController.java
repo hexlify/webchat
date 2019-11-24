7ea23fc31412ad0fad0df1420c01127a55eed3d5
Yegor Druzhinin <e.drujinin@skbkontur.ru>
2019-11-24 21:01:40 +0500

    fix

--- src/main/java/com/webchat/api/WelcomeController.java
+++ src/main/java/com/webchat/api/WelcomeController.java
@@ -3,10 +3,12 @@ package com.webchat.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
     public String index() {
         return "Hello world!";
     }
--- src/main/java/com/webchat/controller/ChatController.java
+++ src/main/java/com/webchat/controller/ChatController.java
@@ -1,18 +1,25 @@
 package com.webchat.controller;
 
 import com.webchat.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class ChatController {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
 
     @MessageMapping("/chat/sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        logger.info("Received: " + chatMessage.getContent());
+
         return chatMessage;
     }
 
