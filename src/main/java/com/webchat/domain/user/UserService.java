5019889ee234d3c31cba342b4cc3c3a06cc50083
Yegor Druzhinin <e.drujinin@skbkontur.ru>
2019-11-24 19:35:44 +0500

    Удалил ненужные компоненты

--- src/main/java/com/webchat/domain/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.webchat.domain.user;
-
-public interface UserService {
-    boolean tryRegister(User user, String password);
-
-    boolean verifyPassword(User user, String password);
-
-    User findByUsername(String username);
-}
