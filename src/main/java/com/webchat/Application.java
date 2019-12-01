41260f7a2558ba25591c348c05b27ffa69e7c6c6
Yegor Druzhinin <e.drujinin@skbkontur.ru>
2019-12-01 15:04:26 +0500

    Добавил дебаг SQL запросов

--- src/main/java/com/webchat/Application.java
+++ src/main/java/com/webchat/Application.java
@@ -3,6 +3,11 @@ package com.webchat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 
 @ServletComponentScan
@@ -11,6 +16,14 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters()
+                .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
     }
 }
 
